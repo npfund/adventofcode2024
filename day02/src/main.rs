1c1307f373deed671d25223c0454d01168d1074c
Nick Pfundstein <nickpfundstein@gmail.com>
2023-12-02 10:27:42 -0700

    Day 2 Part 2 complete

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -19,15 +20,58 @@ fn part1() {
                 let (count, color) = cubes.split_once(' ').unwrap();
                 let count: i32 = count.parse().unwrap();
                 match color {
-                    "red" => if count > 12 { continue 'line },
-                    "green" => if count > 13 { continue 'line },
-                    "blue" => if count > 14 { continue 'line },
-                    _ => panic!()
+                    "red" => {
+                        if count > 12 {
+                            continue 'line;
+                        }
+                    }
+                    "green" => {
+                        if count > 13 {
+                            continue 'line;
+                        }
+                    }
+                    "blue" => {
+                        if count > 14 {
+                            continue 'line;
+                        }
+                    }
+                    _ => panic!(),
                 }
             }
         }
 
         sum += id;
+    }
+
+    println!("{sum}");
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut sum = 0;
+    for line in file.lines().map(|l| l.unwrap()) {
+        let (_, handfuls) = line.split_once(": ").unwrap();
+
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+        for handful in handfuls.split("; ") {
+            for cubes in handful.split(", ") {
+                let (count, color) = cubes.split_once(' ').unwrap();
+                let count: i32 = count.parse().unwrap();
+                match color {
+                    "red" => red = red.max(count),
+                    "green" => green = green.max(count),
+                    "blue" => blue = blue.max(count),
+                    _ => panic!(),
+                }
+            }
+        }
+
+        let power = red * green * blue;
+
+        sum += power;
     }
 
     println!("{sum}");
