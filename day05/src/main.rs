449345da952e52bf1872dcd06248aa47dcf71fb9
Nick Pfundstein <nickpfundstein@gmail.com>
2023-12-05 09:43:23 -0700

    Day 5 Part 2 complete

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -10,6 +10,7 @@ struct MapEntry {
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -54,6 +55,57 @@ fn part1() {
         }
         if value < lowest {
             lowest = value;
+        }
+    }
+
+    println!("{lowest}");
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut seeds: Vec<(i64, i64)> = Vec::new();
+    let mut maps = Vec::new();
+    let mut current = Vec::new();
+    for line in file.lines().map(|l| l.unwrap()) {
+        if line.contains("seeds") {
+            let (_, s) = line.split_once(": ").unwrap();
+            let s: Vec<_> = s.split(' ').map(|s| s.trim().parse().unwrap()).collect();
+            seeds = s.chunks(2).map(|c| (c[0], c[1])).collect();
+        } else if line.contains("map") {
+            if !current.is_empty() {
+                maps.push(current.clone());
+                current.clear();
+            }
+        } else if line.is_empty() {
+            continue;
+        } else {
+            let parts: Vec<i64> = line.split(' ').map(|p| p.parse().unwrap()).collect();
+            current.push(MapEntry {
+                source_offset: parts[1],
+                destination_offset: parts[0],
+                range: parts[2],
+            });
+        }
+    }
+    maps.push(current);
+
+    let mut lowest = i64::MAX;
+    for seed_pair in seeds {
+        for seed in seed_pair.0..seed_pair.0 + seed_pair.1 {
+            let mut value = seed;
+            for map in &maps {
+                for entry in map {
+                    if value >= entry.source_offset && value < entry.source_offset + entry.range {
+                        let delta = value - entry.source_offset;
+                        value = entry.destination_offset + delta;
+                        break;
+                    }
+                }
+            }
+            if value < lowest {
+                lowest = value;
+            }
         }
     }
 
