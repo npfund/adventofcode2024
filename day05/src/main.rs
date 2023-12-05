c73f2ba8370b49e96ce4913782e6e42d2d95aba7
Nick Pfundstein <nickpfundstein@gmail.com>
2023-12-05 09:23:00 -0700

    Day 5 Part 1 complete

---
+++ day05/src/main.rs
@@ -0,0 +1,61 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Clone, Copy, Debug)]
+struct MapEntry {
+    source_offset: i64,
+    destination_offset: i64,
+    range: i64,
+}
+
+fn main() {
+    part1();
+}
+
+fn part1() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut seeds: Vec<i64> = Vec::new();
+    let mut maps = Vec::new();
+    let mut current = Vec::new();
+    for line in file.lines().map(|l| l.unwrap()) {
+        if line.contains("seeds") {
+            let (_, s) = line.split_once(": ").unwrap();
+            seeds = s.split(' ').map(|s| s.trim().parse().unwrap()).collect();
+        } else if line.contains("map") {
+            if !current.is_empty() {
+                maps.push(current.clone());
+                current.clear();
+            }
+        } else if line.is_empty() {
+            continue;
+        } else {
+            let parts: Vec<i64> = line.split(' ').map(|p| p.parse().unwrap()).collect();
+            current.push(MapEntry {
+                source_offset: parts[1],
+                destination_offset: parts[0],
+                range: parts[2],
+            });
+        }
+    }
+    maps.push(current);
+
+    let mut lowest = i64::MAX;
+    for seed in seeds {
+        let mut value = seed;
+        for map in &maps {
+            for entry in map {
+                if value >= entry.source_offset && value < entry.source_offset + entry.range {
+                    let delta = value - entry.source_offset;
+                    value = entry.destination_offset + delta;
+                    break;
+                }
+            }
+        }
+        if value < lowest {
+            lowest = value;
+        }
+    }
+
+    println!("{lowest}");
+}
