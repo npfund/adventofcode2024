2de5aa8d9215d6d044f2c016c671c29c94a010f0
Nick Pfundstein <nickpfundstein@gmail.com>
2024-12-01 12:28:39 -0700

    Day 1 Part 2 complete

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -22,9 +24,31 @@ fn part1() {
 
     let sum = left
         .into_iter()
-        .zip(right.into_iter())
+        .zip(right)
         .map(|(l, r)| l.abs_diff(r))
         .sum::<u32>();
+
+    println!("{sum}");
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut left = Vec::new();
+    let mut right: HashMap<i32, i32> = HashMap::new();
+
+    for line in file.lines().map(|l| l.unwrap()) {
+        let (l, r) = line.split_once("   ").unwrap();
+        left.push(l.parse::<i32>().unwrap());
+        *right.entry(r.parse::<i32>().unwrap()).or_default() += 1;
+    }
+
+    left.sort_unstable();
+
+    let sum = left
+        .iter()
+        .map(|l| l * right.get(l).copied().unwrap_or_default())
+        .sum::<i32>();
 
     println!("{sum}");
 }
