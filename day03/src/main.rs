adbd29776d2c8e838f389d59e8e1b65b3f8116cd
Nick Pfundstein <nickpfundstein@gmail.com>
2024-12-03 13:28:02 -0700

    Day 3 Part 2 complete

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -17,6 +18,34 @@ fn part1() {
         let rhs = rhs.parse::<i32>().unwrap();
 
         sum += lhs * rhs;
+    }
+
+    println!("{sum}");
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let instructions = file.lines().map(|l| l.unwrap()).collect::<String>();
+    let everything = Regex::new(r#"do\(\)|don't\(\)|mul\(\d+,\d+\)"#).unwrap();
+    let mul = Regex::new(r#"mul\((\d+),(\d+)\)"#).unwrap();
+    let mut enabled = true;
+    let mut sum = 0;
+    for m in everything.find_iter(&instructions) {
+        match m.as_str() {
+            "do()" => enabled = true,
+            "don't()" => enabled = false,
+            s => {
+                if enabled {
+                    let (_, [lhs, rhs]) = mul.captures(s).unwrap().extract();
+
+                    let lhs = lhs.parse::<i32>().unwrap();
+                    let rhs = rhs.parse::<i32>().unwrap();
+
+                    sum += lhs * rhs;
+                }
+            }
+        }
     }
 
     println!("{sum}");
